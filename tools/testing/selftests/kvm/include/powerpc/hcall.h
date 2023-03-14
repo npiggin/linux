40dd198f0e29a6ca9c2dd51e7c073363d44b0ab9
Nicholas Piggin <npiggin@gmail.com>
2023-03-14 22:27:13 +1000

    KVM: PPC: selftests: add selftests sanity tests
    Add tests that exercise very basic functions of the kvm selftests
    framework, guest creation, ucalls, hcalls, copying data between guest
    and host, interrupts and page faults.
    
    These don't stress KVM so much as being useful when developing support
    for powerpc.
    
    Acked-by: Michael Ellerman <mpe@ellerman.id.au> (powerpc)
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

--- tools/testing/selftests/kvm/include/powerpc/hcall.h
+++ tools/testing/selftests/kvm/include/powerpc/hcall.h
@@ -12,6 +12,8 @@
 #define UCALL_R4_UCALL	0x5715 // regular ucall, r5 contains ucall pointer
 #define UCALL_R4_SIMPLE	0x0000 // simple exit usable by asm with no ucall data
 
+#define H_RTAS		0xf000
+
 int64_t hcall0(uint64_t token);
 int64_t hcall1(uint64_t token, uint64_t arg1);
 int64_t hcall2(uint64_t token, uint64_t arg1, uint64_t arg2);
